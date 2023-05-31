d3d242b7e84dcd4f25504383c7e45a30e056b808
Ashleyg5 <ashleygiraldo5@hotmail.com>
2023-05-31 09:09:18 -0400

    added contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 export default function Contact() {
   const [validated, setValidated] = useState(false);
@@ -37,18 +37,20 @@ export default function Contact() {
     setFormData({ ...formData, email });
     setIsEmailValid(event.target.checkValidity());
   };
-
+//! align center
   return (
-    <div>
-      <h1 className="m-3">Contact Page</h1>
+  <section className='container'>
+    <h5 className='mb-0 mt-3 text-center'>Get in Touch</h5>
+      <h1 className='text-center'>Contact Me</h1>
+    <div> 
       <Form noValidate validated={validated} onClick={handleSubmit}>
-        <Row className="m-3">
-          <Form.Group as={Col} md="4" controlId="validationCustom01">
+        <Row>
+          <Form.Group as={Col} md="5" controlId="validationCustom01">
             <Form.Label>Name: </Form.Label>
             <Form.Control
               required
               type="text"
-              placeholder="Name"
+              placeholder="Your name"
               value={formData.name}
               onChange={(e) => setFormData({ ...formData, name: e.target.value })}
             />
@@ -57,13 +59,13 @@ export default function Contact() {
             </Form.Control.Feedback>
           </Form.Group>
         </Row>
-        <Row className="m-3">
-          <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+        <Row className='mt-3'>
+          <Form.Group as={Col} md="5" controlId="validationCustomUsername">
             <Form.Label>Email Address: </Form.Label>
             <InputGroup hasValidation>
               <Form.Control
                 type="email"
-                placeholder="Email"
+                placeholder="Your email"
                 aria-describedby="janedoe@email.com"
                 required
                 value={formData.email}
@@ -75,18 +77,36 @@ export default function Contact() {
             </InputGroup>
           </Form.Group>
         </Row>
-        <Row className="m-3">
-          <Form.Group as={Col} md="6" controlId="validationCustom03">
+        <Row className='mt-3'>
+          <Form.Group as={Col} md="5" controlId="validationCustom03">
             <Form.Label>Message: </Form.Label>
-            <Form.Control type="textarea" placeholder="Your message here" required value={formData.message}
+            <Form.Control as="textarea" style={{ height: '100px' }} placeholder="Your message" required value={formData.message}
               onChange={(e) => setFormData({ ...formData, message: e.target.value })} />
             <Form.Control.Feedback type="invalid">
               Message is required.
             </Form.Control.Feedback>
           </Form.Group>
         </Row>
-        <Button className="m-4" type="submit" id='register' disabled="disabled">Submit form</Button>
+        <span className="btn" type="submit" id='register' disabled="disabled">Send Message</span>
       </Form>
     </div>
+    <div className="d-flex flex-column align-items-end mt-3 ms-auto">
+        <Button type='email' href='mailto: ashleygiraldo5@hotmail.com' variant="primary" size="lg">
+          EMAIL
+          <hr />
+          Ashleygiraldo5@hotmail.com
+        </Button>{' '}
+        <Button type='link' href='https://github.com/Ashleyg5' variant="secondary" size="lg">
+          GITHUB
+          <hr />
+          Ashleyg5
+        </Button>{' '}
+        <Button type='link' href='https://www.linkedin.com/in/ashley-giraldo' variant="secondary" size="lg">
+          LINKEDIN
+          <hr />
+          Ashley Giraldo
+        </Button>
+      </div>
+  </section>
   );
 }
